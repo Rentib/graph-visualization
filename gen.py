29dfcd40ba34ed05768130c9e4500b663f02c005
Rentib <sbitner420@tutanota.com>
2024-10-13 12:57:43 +0200

    refactor(gen): clean up code

--- gen.py
+++ gen.py
@@ -10,20 +10,25 @@ plt.rcParams.update({
     "text.usetex": True,          # Use LaTeX for text rendering
 })
 
-G = nx.Graph()
-G.add_edges_from([
-    (1, 2), (1, 3), (2, 4), (2, 5), (2, 6),
-    (3, 5), (3, 6), (4, 7), (4, 8), (5, 7),
-    (5, 8), (6, 7), (6, 8), (7, 9), (8, 9)
-])
-pos = nx.bfs_layout(G, 1)
+def draw_graph(G, pos, filename):
+    nx.draw(G,
+            pos,
+            with_labels=True,
+            node_color='white',
+            edgecolors='black',
+            node_size=1000,
+            edge_color='black')
+    plt.savefig(filename)
+    plt.close()
 
-nx.draw(G,
-        pos,
-        with_labels=True,
-        node_color='white',
-        edgecolors='black',
-        node_size=1000,
-        edge_color='black')
+def gen_fig1():
+    G = nx.Graph()
+    G.add_edges_from([
+        (1, 2), (1, 3), (2, 4), (2, 5), (2, 6),
+        (3, 5), (3, 6), (4, 7), (4, 8), (5, 7),
+        (5, 8), (6, 7), (6, 8), (7, 9), (8, 9)
+    ])
+    pos = nx.bfs_layout(G, 1)
+    draw_graph(G, pos, "./figures/fig1.pgf")
 
-plt.savefig("./figures/fig1.pgf", format="pgf")
+gen_fig1()
